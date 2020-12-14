b70133f7f4166ed6d13629efd1770c471e12000e
Leo Pieren <lpi114107@iet-gibb.ch>
2020-12-14 14:46:25 +0100

    drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { toDo, inProgress, done } from "./data";
+import { AppModule } from "./app.module"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'taskBoard';
+  tasksToDo = [
+    "finish this board",
+    "reply to all emails"
+  ]
+  tasksInProgress = [
+    "test 1",
+    "test 2"
+  ]
+  tasksDone = [
+    "test 3",
+    "test 4"
+  ]
+
+  constructor() {
+
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
 }
+
---
+++ src/app/data.ts
@@ -0,0 +1,21 @@
+export const toDo = [{
+    "name": "finish this kanban board",
+    "description": "blah blah"
+  },
+
+  {
+    "name": "test",
+    "description": "blah blah"
+  },
+
+];
+
+export const inProgress = [{
+  "name": "mathe lernen",
+  "description": ":("
+}];
+
+export const done = [{
+  "name": "lernbericht schreiben",
+  "description": "blah blah"
+}];
